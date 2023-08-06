0c466113a58d7f208e96af7b009aa63fa381c748
omerfkgoze <farukucukgoze@gmail.com>
2023-08-06 18:35:47 +0200

    new project

---
+++ script.js
@@ -0,0 +1,153 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+// let map, mapEvent; //*! Bu global değişkenlerin yerine App class'ı içinde private instance properties tanımladık.
+
+///////////////////////////////////////
+
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10); // Date.now() fonksiyonu milisaniye cinsinden tarih döndürür. Bu tarihin son 10 hanesini alıyoruz. Bu sayede id'lerimiz unique olacak. Normalde bu durum için bir paket kullanılır. Ancak biz burada bu şekilde bir çözüm bulduk.
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // km
+    this.duration = duration; // min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace(); // Pace: hız
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed(); // Speed: hız
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// DENEME
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycling1);
+
+///////////////////////////////////////
+
+//* APPLICATION ARCHITECTURE
+
+class App {
+  // private instance properties
+  #map;
+  #mapEvent;
+
+  constructor() {
+    this._getPosition(); //*? class dışında app._getPosition() şeklinde çağrılabilir. Ancak constructor içinde çağrıldığında da aynı sonucu alabiliriz çünkü sayfa yüklendiğinde constructor otomatik olarak çalışacaktır, biz de bundan faydalanmış oluruz.
+
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      // ilk callback fonksiyonu konum başarılı olduğunda ikinci callback fonksiyonu ise konum başarısız olduğunda çalışır.
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        }
+      );
+    }
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+
+    // getCurrentPosition() fonksiyonu içinde this keywordünü kullanırsak bu fonksiyonu çağıran objeyi yani navigator.geolocation objesini gösterir. Ancak bizim istediğimiz this keywordünü App classını göstermesidir. Bu yüzden bu fonksiyonu çağırırken bind() methodunu kullanırız.
+    console.log(this);
+    this.#map = L.map('map').setView(coords, 13);
+    // console.log(map);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // Handling clicks on map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    // Display marker
+    console.log(this.#mapEvent);
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
+}
+
+const app = new App();
